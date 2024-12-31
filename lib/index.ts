ac7b10cadf7290ab027624593aa516bfb1e7e55b
Doomester <doomester@duck.com>
2024-12-31 21:12:58 +0000

    some things

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
-'use strict';
-
+export type * from "./types/index";
 export { default as PWApiClient } from "./api/PWApiClient.js";
 export { default as PWGameClient } from "./game/PWGameClient.js";
 export * as Constants from "./util/Constants.js";
+
+export * from "./util/Constants.js";
