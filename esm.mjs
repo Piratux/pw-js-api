d52edf3502a2a105bd3bc4e2f0f79e3cfbe08c60
Doomester <doomester@duck.com>
2025-01-12 01:03:56 +0000

    export proto

--- esm.mjs
+++ esm.mjs
@@ -6,11 +6,13 @@ if ("default" in PWApiClient) PWApiClient = PWApiClient.default;
 
 const Constants = (await import("./dist/util/Constants.js")).default;
 const BlockNames = (await import("./dist/util/block.js")).BlockNames;
+const ProtoGen = (await import("./dist/gen/world_pb.js")).default;
+
 
 export default {
-    PWGameClient, PWApiClient, Constants, BlockNames
+    PWGameClient, PWApiClient, Constants, BlockNames, ProtoGen
 };
 
 export {
-    PWGameClient, PWApiClient, Constants, BlockNames
+    PWGameClient, PWApiClient, Constants, BlockNames, ProtoGen
 };
--- lib/index.ts
+++ lib/index.ts
@@ -4,3 +4,4 @@ export { default as PWGameClient } from "./game/PWGameClient.js";
 export * as Constants from "./util/Constants.js";
 export * as Errors from "./util/Errors.js";
 export { BlockNames } from "./util/block.js";
+export * as ProtoGen from "./gen/world_pb.js";
