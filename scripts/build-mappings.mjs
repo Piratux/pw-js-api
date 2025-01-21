964d4daa875480f96ef3d720fc3749fead6ff0c5
Piratux <58703216+Piratux@users.noreply.github.com>
2025-01-21 22:37:40 +0200

    test

--- scripts/build-mappings.mjs
+++ scripts/build-mappings.mjs
@@ -12,7 +12,7 @@ console.log("pw-js-api-fork build-mappings")
 console.log("pw-js-api-fork build-mappings")
 
 
-let tsOutput = "export const enum BlockNames {\n";
+let tsOutput = "export enum BlockNames {\n";
 
 for (let i = 0, len = entries.length; i < len; i++) {
     tsOutput += "   " + entries[i][0].toUpperCase() + " = " + entries[i][1] + ",\n"
