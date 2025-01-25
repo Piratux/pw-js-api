135fae2a59ed90cc4f7d6d07819dae76fa428eb6
Doomester <doomester@duck.com>
2025-01-25 21:42:45 +0000

    Exports all constants now

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 export type * from "./types/index.d.ts";
 export { default as PWApiClient } from "./api/PWApiClient.js";
 export { default as PWGameClient } from "./game/PWGameClient.js";
+export * from "./util/Constants.js";
 export * as Constants from "./util/Constants.js";
 export * as Errors from "./util/Errors.js";
 export { BlockNames } from "./util/block.js";
