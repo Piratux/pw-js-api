45ad96085ae9bd2c4434af1d5e6595acb344d0b4
Doomester <doomester@duck.com>
2025-07-20 22:32:21 +0100

    forgot to export atlases

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 export type * from "./types/index.d.ts";
 export { default as PWApiClient } from "./api/PWApiClient.js";
 export { default as PWGameClient } from "./game/PWGameClient.js";
+export { default as PWAtlases } from "./api/PWAtlases.js";
 export * from "./util/Constants.js";
 export * as Constants from "./util/Constants.js";
 export * as Errors from "./util/Errors.js";
