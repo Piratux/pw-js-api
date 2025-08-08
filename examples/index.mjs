7fa7b710547f0ad5c2d1896248fa90beb64c40ef
Doomester <doomester@duck.com>
2025-08-08 23:04:46 +0100

    Updates the example

--- examples/index.mjs
+++ examples/index.mjs
@@ -1,74 +1,73 @@
-// Up to date as of v0.0.5
-// - 2025-01-01 21:37 UTC+0
+// Up to date as of v0.3.11
+// - 2025-08-08 21:57 UTC+00
 
-import { PWApiClient } from "pw-js-api";
-import { swapObject } from "./util.mjs";
+import {PWApiClient, PWGameClient } from "pw-js-api";
 
-import { setTimeout } from "node:timers/promises";
+// Optional package, it takes care of player/world states for you, even blocks / structures.
+// For more information: https://github.com/doomestee/pw-js-world
+import { Block, createBlockPacket, createBlockPackets, LayerType, PWGameWorldHelper } from "pw-js-world";
 
-const cli = new PWApiClient("<YOUR EMAIL HERE>", "<YOUR PASSWORD HERE>");
+// Create an API client with your email and password, or token if you wish.
+const api = new PWApiClient("EMAIL@MAIL.AIL", "PASSWORD");
 
-const mappings = await cli.getMappings();
-const blockIdToType = swapObject(mappings);
+// You must run this if you used email/password, or if the token is expired before using authenticated requests (including joining worlds)
+await api.authenticate();
 
-// To fetch the token, allowing it to join a world.
-await cli.authenticate();
+// This will allow us to be able to use the world's states at any time once it's added to the game connection.
+const helper = new PWGameWorldHelper();
 
-const con = await cli.joinWorld("rfdeeb8d9d94f76", {
-    gameSettings: {
-        handlePackets: ["PING"]
+// Creates a game client (this is one of two options, this option allows us to add hooks/callbacks before the joinWorld resolves)
+const con = new PWGameClient(api, {
+    handlePackets: ["PING", "INIT"]
+}).addHook(helper.receiveHook);
+
+// You can use addCallback
+// If a hook is added to PWGameClient, the states will be populated (though it can be undefined if it errors during processing).
+con.addCallback("playerInitPacket", (evt, states) => {
+    console.log("Joined " + evt.worldMeta.title + " as " + evt.playerProperties.username);
+
+    if (states?.player) {
+        console.log("Bot can " + (states.player.rights.canEdit ? "" : "not ") + "edit the world.");
+    }
+}) // which can return itself for chaining
+.addCallback("playerJoinedPacket", (data, states) => {
+    if (states.player) {
+        const p = states.player;
+
+        if (p.playerId < helper.botPlayerId) {
+            console.log(`${p.username} is here.`);
+        } else console.log(`${p.username} joined the world!`);
     }
 });
 
-/**
- * Doesn't support filling
- * @param {string} blockId 
- * @param {{ x: number, y: number }[]} positions 
- * @param {0|1} layer 
- * @returns 
- */
-const place = (blockId, positions, layer = 1) => {
-    return con.send("worldBlockPlacedPacket", {
-        blockId: mappings[blockId], layer,
-        positions, isFillOperation: false
-    });
-}
-
-let botId = 1; let isOwner = false;
-
-// You can use setCallBack (which returns itself for chaining)
-con.addCallback("playerInitPacket", (data) => {
-    console.log("Connected as " + data.playerProperties?.username);
-
-    if (data.playerProperties) {
-        botId = data.playerProperties.playerId;
-        isOwner = data.playerProperties.isWorldOwner;
-    }
-
-    con.send("playerInitReceived");
-}).addCallback("playerJoinedPacket", (data) => {
-    console.log(data.properties?.username + (data.properties.playerId < botId ? " is here." : " joined the world."));
-});
-
+// Comment this out if you do not want your console to be flooded for every request received.
 con.addCallback("debug", console.log);
 
-const snakeable = ["basic_", "brick_", "beveled_", "glass_", "minerals_"];
+con.addCallback("worldBlockPlacedPacket", async function (data, states) {
+    if (states === undefined || states.player === undefined) return;
 
-con.addCallback("worldBlockPlacedPacket", async function (data) {
-    if (data.playerId === botId) return;
+    const { player } = states;
 
-    if (isOwner) {
-        const blockId = blockIdToType[data.blockId];
+    if (player.isMe || !helper.botPlayer.rights.canEdit) return;
 
-        if (data.isFillOperation) return; // fills have long cooldown.
+    const positions = [];
+    const block = states.newBlocks[0];
 
-        if (snakeable.some(v => blockId === v + "green" || blockId === v + "red")) {
-            if (blockId.endsWith("green")) {
-                await setTimeout(150).then(() => place(blockId.split("_")[0] + "_red", data.positions, 1));
-            }
-            await setTimeout(300).then(() => place("empty", data.positions, 1));
-        }
+    if (block.bId !== Block.getIdByName("BASIC_GREEN")) return;
+
+    for (let i = 0, len = data.positions.length; i < len; i++) {
+        positions.push(data.positions[i]);
     }
+
+    setTimeout(() => {
+        con.send("worldBlockPlacedPacket", createBlockPacket(new Block(Block.getIdByName("BASIC_RED")), LayerType.Foreground, positions));
+
+
+        setTimeout(() => {
+            con.send("worldBlockPlacedPacket", createBlockPacket(new Block(Block.getIdByName("EMPTY")), LayerType.Foreground, positions));
+        }, 175);
+
+    }, 150);
 });
 
 let helpTime = -1;
@@ -89,7 +88,7 @@ con.addCallback("systemMessagePacket", (data) => {
 // All messages will get logged if and only if the person isn't the bot.
 // This can be tested with the ".say .ping" command where you will notice that the bot's messages does not get logged nor will it be listened to.
 con.addCallback("playerChatPacket", (data) => {
-    if (data.playerId === botId) return "STOP";
+    if (data.playerId === helper.botPlayerId) return "STOP";
 
     console.log(data.message);
 });
@@ -116,3 +115,5 @@ con.addCallback("playerChatPacket", (data) => {
             break;
     }
 });
+
+await con.joinWorld("rfdeeb8d9d94f76");
